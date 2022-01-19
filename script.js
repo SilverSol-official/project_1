a9d948e595d35457ee9f4e9841cba766a0698a02
Pupa <silversol3301@gmail.com>
2022-01-19 12:07:38 +0200

    First serious practice

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
-console.log("hello world!!!")
+"use strict";
+let numberOfFilms=prompt("Сколько фильмов вы просмотрели ?");
+let personalMovieDB={
+    count : numberOfFilms,
+    movies : {},
+    actors : {},
+    genres : [],
+    privat : false
+}
+let name=prompt("Один из просмотренных фильмов?"),
+    mark=prompt("На сколько оценете его?");
+    movies[name,mark];
+    console.log(movies);
